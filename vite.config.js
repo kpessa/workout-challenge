4a7c4d0ecdd02ecd73411206f6d6c6f274d80c2b
Kurt Pessa <kpessa@gmail.com>
2024-12-22 16:50:23 -0500

    got app to work

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   base: './',
