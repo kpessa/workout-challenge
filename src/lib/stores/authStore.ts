116addaab7407525599706fdd9d62cd2779c9e2a
Kurt Pessa <kpessa@gmail.com>
2025-01-01 14:07:48 -0500

    trying to get google verication working

--- src/lib/stores/authStore.ts
+++ src/lib/stores/authStore.ts
@@ -56,7 +56,9 @@ function createAuthStore() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`
+          redirectTo: typeof window !== 'undefined' 
+            ? `${window.location.origin}/auth/callback`
+            : undefined
         }
       });
 
